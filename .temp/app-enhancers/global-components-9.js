553f84846da8277e893f8ea264a5ddd3524dc005
zhao <1062848999@qq.com>
2021-09-27 21:56:32 +0800

    a

--- .temp/app-enhancers/global-components-9.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-
-Vue.component("Badge", () => import("D:\\vuePress\\node_modules\\@vuepress\\theme-default\\global-components\\Badge"))
-Vue.component("CodeBlock", () => import("D:\\vuePress\\node_modules\\@vuepress\\theme-default\\global-components\\CodeBlock"))
-Vue.component("CodeGroup", () => import("D:\\vuePress\\node_modules\\@vuepress\\theme-default\\global-components\\CodeGroup"))
-
-
-export default {}
